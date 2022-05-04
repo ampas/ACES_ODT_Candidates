a43a425c2ebf54e0f3d669cc6a6c128c24e69ebe
Alex Fry <afry@ilm.com>
2022-05-05 07:12:55 +1000

    license update

--- python/lutWriteNodeCode.py
+++ python/lutWriteNodeCode.py
@@ -1,8 +1,17 @@
 ### This code is embedded in the nukescript, not accessed from here
 
 
+
+
+
 import os
 import shutil
+
+
+node = nuke.thisNode()
+
+
+bakeLuts = nuke.thisNode().knob('bakeLUTs').value()
 
 
 def cubeToCub(cubePath,cubPath):
@@ -10,9 +19,15 @@ def cubeToCub(cubePath,cubPath):
     ouputCubPath = cubPath
 
 
+
+
+
     # read the cube
     with open(inputCubePath) as f:
         lines = f.readlines()
+
+
+
 
 
     # header
@@ -23,8 +38,14 @@ def cubeToCub(cubePath,cubPath):
     # Cube'''
 
 
+
+
+
     # footer
     tlFooter = '''\n# end\n'''
+
+
+
 
 
 
@@ -32,6 +53,9 @@ def cubeToCub(cubePath,cubPath):
     LUTlines = [x.replace('\n','').split(' ') for x in LUTlines]
     LUTlines = [[float(i) for i in x] for x in LUTlines]
     LUTlines = [[str(i) for i in x] for x in LUTlines]
+
+
+
 
 
     ## remove decimal from 0.0 and 1.0
@@ -43,6 +67,9 @@ def cubeToCub(cubePath,cubPath):
                 LUTlines[i][j] = '1'
 
 
+
+
+
     # reconstruct the cube string
     stringLUTlines = []
     for line in LUTlines:
@@ -50,8 +77,14 @@ def cubeToCub(cubePath,cubPath):
     newLUTStringBlock = '\n'.join(stringLUTlines)
 
 
+
+
+
     # join it all up
     outputContents = tlHeader + '\n' + newLUTStringBlock + '\n' + tlFooter
+
+
+
 
 
     # check directory for outputCubPath exists, if not, create it
@@ -59,15 +92,24 @@ def cubeToCub(cubePath,cubPath):
         os.makedirs(os.path.dirname(ouputCubPath))
 
 
+
+
+
     # write the file
     with open(ouputCubPath, 'w') as f:
         f.write(outputContents)
+
+
+
 
 
 def bakeCandidateLUTfromNode(node):
     candidate = node.knob('candidate').value()
     revision = node.knob('revision').value()
     target = node.knob('target').value()
+
+
+
 
 
     nukeScriptDir = nuke.script_directory()
@@ -78,6 +120,9 @@ def bakeCandidateLUTfromNode(node):
     print(flSpacePath)
 
 
+
+
+
     cubePath = os.path.join(nukeScriptDir,node.knob('cubePath').evaluate())
     ocioCubePath = os.path.join(nukeScriptDir,node.knob('ocioCubePath').evaluate())
     cubPath = os.path.join(nukeScriptDir,node.knob('cubPath').evaluate())
@@ -85,20 +130,35 @@ def bakeCandidateLUTfromNode(node):
     node.knob('file').setValue(cubePath)
 
 
+
+
+
     # check cubePath exists, if not, create it
     if not os.path.exists(os.path.dirname(cubePath)):
         os.makedirs(os.path.dirname(cubePath))
+
+
+
 
     # check ocioCubePath exists, if not, create it
     if not os.path.exists(os.path.dirname(ocioCubePath)):
         os.makedirs(os.path.dirname(ocioCubePath))
 
 
+
+
+
     dctlPath = cubePath.replace('.cube','.dctl')
     cubeName = cubePath.split('/').pop(-1)
 
 
-    node.knob('generate').execute()
+
+
+    if bakeLuts == True:
+        node.knob('generate').execute()
+
+
+
 
 
     ## write the dctl file
@@ -109,12 +169,21 @@ def bakeCandidateLUTfromNode(node):
         f.write(''.join(newLines))
 
 
+
+
+
     # Baselight
     # convert cube to cub
     cubeToCub(cubePath,cubPath)
 
+
+
+
     # copy cubePath to ocioCubePath
     shutil.copy(cubePath,ocioCubePath)
+
+
+
 
 
     ## write the fltransform
@@ -129,13 +198,21 @@ def bakeCandidateLUTfromNode(node):
             f.write(''.join(newLines))
 
 
+
+
+
     # check ACEScct_AP0.flspace exists, if not, copy it
-    ACEScct_AP0_flspace_path = os.path.dirname(cubPath) + '/ACEScct_AP0.flspace'
-    if not os.path.exists(ACEScct_AP0_flspace_path):
-        shutil.copy(flSpacePath,ACEScct_AP0_flspace_path)
+    for flspace in ['/ACEScct_AP0.flspace','/ACEScct_APS4.flspace']:
+        ACEScct_AP0_flspace_path = os.path.dirname(cubPath) + flspace
+        if not os.path.exists(ACEScct_AP0_flspace_path):
+            shutil.copy(flSpacePath,ACEScct_AP0_flspace_path)
+
+
+
 
 
 def createOCIOconfigs(revision):
+    nukeScriptDir = nuke.script_directory()
     revision = nuke.thisNode().knob('revision').value()
     ocioTemplateDir = os.path.join(nukeScriptDir,'resources/')
     newOcioTemplateDir = os.path.join(nukeScriptDir,'OCIO/')
@@ -151,13 +228,22 @@ def createOCIOconfigs(revision):
         with open(newConfigPath, 'w') as f:
             f.write(''.join(newLines))
 
+
+
+
 ODTWrites = []
 for node in nuke.allNodes():
     if 'Write_ResolveACES_ODT_LUT' in node.name():
         ODTWrites.append(node)
 
+
+
+
 for ODTWriteNode in ODTWrites:
     bakeCandidateLUTfromNode(ODTWriteNode)
+
+
+
 
 # createOCIOconfigs(revision)
 createOCIOconfigs(nuke.thisNode().knob('revision').getValue())
